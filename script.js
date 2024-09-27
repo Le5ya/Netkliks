a4eb046e576dd108c9d8de14ccf6274d696fd3e4
Le5ya <liliatolstyh@yandex.ru>
2024-09-27 16:11:49 +0300

    Connecting JS

---
+++ script.js
@@ -0,0 +1,68 @@
+const cardWrapper = document.querySelector(".content-cards");
+const searchButton = document.querySelector("#search-button");
+const searchInput = document.querySelector("#search-input");
+const films = [
+  {
+    id: 0,
+    title: "Марсианин 1",
+    original: "The Martian 2015",
+    category: "Фантастика, приключения",
+    rating: 7.8,
+    link: "/film.html",
+    image: "./images/film.png",
+  },
+  {
+    id: 0,
+    title: "Марсианин 2",
+    original: "The Martian 2015",
+    category: "Фантастика, приключения",
+    rating: 7.8,
+    link: "/film.html",
+    image: "./images/film.png",
+  },
+  {
+    id: 0,
+    title: "Марсианин 3",
+    original: "The Martian 2015",
+    category: "Фантастика, приключения",
+    rating: 7.8,
+    image: "./images/film.png",
+    link: "film.html",
+  },
+];
+const render = (array) => {
+  console.log(array);
+
+  cardWrapper.innerHTML = "";
+
+  films.forEach((item) => {
+    cardWrapper.insertAdjacentHTML(
+      "beforeend",
+      `
+      <a href="${item.link}" class="content-cards__item">
+        <div class="content-cards__item--img">
+          <img src="${item.image}" alt="film">
+        </div>
+        <div class="content-cards__item--title">
+          <h5>${item.title},</h5>
+          <span>${item.original}</span>
+        </div>
+        <p class="content-cards__item--description">
+          ${item.category}
+        </p>
+        <p class="content-cards__item--rating">${item.rating}</p>
+      </a>
+      
+      `
+    );
+  });
+};
+
+searchButton.addEventListener("click", () => {
+  render(
+    films.filter((item) => {
+      return item.title.includes(searchInput.value);
+    })
+  );
+});
+render(films);
